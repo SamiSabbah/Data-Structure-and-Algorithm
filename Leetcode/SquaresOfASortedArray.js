e06faf0d3367e8b1fa5cc13422b09e77b2ff0596
SamiSabbah <samysubbah1@gmail.com>
2022-08-14 11:34:51 +0300

    Squares of a Sorted Array sol

---
+++ Leetcode/SquaresOfASortedArray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = nums[i] * nums[i];
+  }
+
+  let sortedNums = nums.sort((a, b) => a - b);
+
+  return sortedNums;
+};
